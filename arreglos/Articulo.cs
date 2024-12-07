734cb2375d687587fe2979f551bb9b6b5514f50d
unknown <aaronrs3006@gmail.com>
2024-12-06 19:38:22 -0600

    tareaArreglos

---
+++ arreglos/Articulo.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglos
+{
+    internal class Articulo
+    {
+
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public string Bodega { get; set; }
+
+        public Articulo(string codigo, string nombre, decimal precio, int cantidad, string bodega)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Precio = precio;
+            Cantidad = cantidad;
+            Bodega = bodega;
+        }
+
+        public override string ToString()
+        {
+            return $"Código: {Codigo}, Nombre: {Nombre}, Precio: {Precio}, Cantidad: {Cantidad}, Bodega: {Bodega}";
+        }
+    }
+
+    class Inventario
+    {
+        private List<Articulo> articulos = new List<Articulo>();
+
+        public void IngresarArticulo()
+        {
+            Console.Write("Código: ");
+            string codigo = Console.ReadLine();
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Precio: ");
+            decimal precio = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Cantidad: ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bodega: ");
+            string bodega = Console.ReadLine();
+
+            Articulo articulo = new Articulo(codigo, nombre, precio, cantidad, bodega);
+            articulos.Add(articulo);
+            Console.WriteLine("Artículo ingresado correctamente.");
+        }
+
+        public void ModificarArticulo()
+        {
+            Console.Write("Ingrese el código del artículo a modificar: ");
+            string codigo = Console.ReadLine();
+
+            Articulo articulo = articulos.Find(a => a.Codigo == codigo);
+
+            if (articulo != null)
+            {
+                Console.Write("Nuevo nombre: ");
+                articulo.Nombre = Console.ReadLine();
+                Console.Write("Nuevo precio: ");
+                articulo.Precio = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Nueva cantidad: ");
+                articulo.Cantidad = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Nueva bodega: ");
+                articulo.Bodega = Console.ReadLine();
+
+                Console.WriteLine("Artículo modificado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Artículo no encontrado.");
+            }
+        }
+
+        public void BorrarArticulo()
+        {
+            Console.Write("Ingrese el código del artículo a borrar: ");
+            string codigo = Console.ReadLine();
+
+            Articulo articulo = articulos.Find(a => a.Codigo == codigo);
+
+            if (articulo != null)
+            {
+                articulos.Remove(articulo);
+                Console.WriteLine("Artículo eliminado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Artículo no encontrado.");
+            }
+        }
+
+        public void ConsultarArticulo()
+        {
+            Console.WriteLine("Lista de artículos:");
+            foreach (var articulo in articulos)
+            {
+                Console.WriteLine(articulo);
+            }
+        }
+
+        public void BuscarArticulo()
+        {
+            Console.WriteLine("Buscar por:");
+            Console.WriteLine("1. Código");
+            Console.WriteLine("2. Nombre");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            if (opcion == 1)
+            {
+                Console.Write("Ingrese el código: ");
+                string codigo = Console.ReadLine();
+                Articulo articulo = articulos.Find(a => a.Codigo == codigo);
+
+                if (articulo != null)
+                {
+                    Console.WriteLine(articulo);
+                }
+                else
+                {
+                    Console.WriteLine("Artículo no encontrado.");
+                }
+            }
+            else if (opcion == 2)
+            {
+                Console.Write("Ingrese el nombre: ");
+                string nombre = Console.ReadLine();
+                Articulo articulo = articulos.Find(a => a.Nombre == nombre);
+
+                if (articulo != null)
+                {
+                    Console.WriteLine(articulo);
+                }
+                else
+                {
+                    Console.WriteLine("Artículo no encontrado.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opción inválida.");
+            }
+        }
+    }
+
+  
+
+}
---
+++ arreglos/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventario inventario = new Inventario();
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("\nSistema de Gestión de Inventarios");
+                Console.WriteLine("1. Ingresar artículo");
+                Console.WriteLine("2. Modificar artículo");
+                Console.WriteLine("3. Borrar artículo");
+                Console.WriteLine("4. Consultar artículos");
+                Console.WriteLine("5. Buscar artículo");
+                Console.WriteLine("6. Salir");
+                Console.Write("Seleccione una opción: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        inventario.IngresarArticulo();
+                        break;
+                    case 2:
+                        inventario.ModificarArticulo();
+                        break;
+                    case 3:
+                        inventario.BorrarArticulo();
+                        break;
+                    case 4:
+                        inventario.ConsultarArticulo();
+                        break;
+                    case 5:
+                        inventario.BuscarArticulo();
+                        break;
+                    case 6:
+                        Console.WriteLine("Saliendo del sistema...");
+                        break;
+                    default:
+                        Console.WriteLine("Opción inválida. Intente nuevamente.");
+                        break;
+                }
+            } while (opcion != 6);
+        }
+    }
+}
+
